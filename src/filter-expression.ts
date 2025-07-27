a8bbd82b692530a75d999e6c45d70ba6582e73f2
mohammed412anas <mohammed.anas@everest.engineering>
2025-07-28 00:50:29 +0530

    Enclose welcome message in printMessage function

---
+++ src/filter-expression.ts
@@ -0,0 +1,25 @@
+import { takeInput } from "./calculator";
+
+export function printMessage(){
+console.log(`
+Welcome to simple calculator the app have the following features.
+ Basic Operations
+  1.Addition.
+  2.Subtraction.
+  3.Multiplication.
+  4.Division.
+  5.Modulus.
+  6.Power.
+
+ Scientific Functions
+  1.Square Root (sqrt).
+  2.Factorial (!).
+
+ Expression Evaluation
+  1.Evaluate math expression.
+`
+    )
+};
+
+
+
