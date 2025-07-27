fb87a03a7bc3af2c97c6fc90ad7ac9a0bdd6977e
mohammed412anas <mohammed.anas@everest.engineering>
2025-07-27 17:11:09 +0530

    Add initial failing test for takeInput function in calculator

---
+++ test/calculator.test.ts
@@ -0,0 +1,18 @@
+import PromptSync from "prompt-sync";
+import { takeInput } from "../src/calculator";
+
+
+jest.mock("prompt-sync");
+let mockedPrompt : jest.Mock = jest.fn();
+(PromptSync as jest.Mock).mockReturnValue(mockedPrompt);
+
+describe("calculator",()=>{
+    test("should fail",()=>{
+        mockedPrompt.mockReturnValueOnce("10+20");
+
+        let input : string = takeInput();
+
+        expect(input).toEqual("20+10");
+    
+    });
+});
