ff01e665c3dfb2dba145c92d10da731dca68149e
mohammed412anas <mohammed.anas@everest.engineering>
2025-07-28 19:04:50 +0530

    Add unit test for mathOperations to perform addition when user selects '1'

---
+++ test/main.test.ts
@@ -0,0 +1,20 @@
+import { exp } from "mathjs";
+import {mathOpreations} from "../src/main" ;
+import PromptSync from "prompt-sync";
+
+jest.mock("prompt-sync")
+let mockedPrompt : jest.Mock = jest.fn();
+(PromptSync as jest.Mock).mockReturnValue(mockedPrompt);
+
+
+describe("test suit of main.ts module",()=>{
+    test("should take first mocked prompt value as '1' and perform addition between '0' and mocked prompt value '5' and return '5'",()=>{
+        mockedPrompt
+            .mockReturnValueOnce("1")
+            .mockReturnValueOnce("5");
+
+        let sum : number = mathOpreations()
+
+        expect(sum).toEqual(5)
+    })
+})
