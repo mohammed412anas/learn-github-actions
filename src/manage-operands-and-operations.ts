2416f2098dc9d43c37b47f105d44b4769e6c90f0
mohammed412anas <mohammed.anas@everest.engineering>
2025-07-29 13:38:38 +0530

    Remove mistakenly wrote test cases in main.test.ts which are related to different module

--- src/manage-operands-and-operations.ts
+++ src/manage-operands-and-operations.ts
@@ -1,6 +1,32 @@
 import { takeInput } from "./calculator";
 
-
+export function manageOperendsAndOperations(oper:string,
+    callBack:(a:number,b:number)=>number
+):number{
+    
+    var index : number = 1;
+    var initialSum : number = 0;
+    let input :string = takeInput(`Input ${index} to perform ${oper} : `);
+    performOperation()
+    function performOperation(){
+        if(!isNaN(Number(input))){
+            let validatedResult : number|string = validateOprends(input,oper,index,initialSum);
+            let value :number = validatedResult as number;
+            console.log(`Result obtained by performing ${oper}`,
+                ` between ${initialSum} and ${value} : `,
+                `${initialSum = callBack(initialSum,value)}`);
+            index++;
+            input = takeInput(`Input ${index} to perform ${oper} : `);
+            return performOperation()
+        }else if(input.toLowerCase() === "exit"){
+            return initialSum;
+        }else{
+            input = takeInput(`Input ${index} to perform ${oper} : `);
+            return performOperation();
+        }
+    }
+    return initialSum;
+}
 export function validateOprends(input:string,oper:string,index : number,initialSum:number):number|string{
     if(isNaN(Number(input))){
         if(input.toLowerCase() === "exit"){
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,6 +1,5 @@
 import {mathOpreations} from "../src/main" ;
 import PromptSync from "prompt-sync";
-import { validateOprends } from "../src/manage-operands-and-operations";
 
 jest.mock("prompt-sync")
 let mockedPrompt : jest.Mock = jest.fn();
@@ -17,46 +16,6 @@ describe("test suit of main.ts module",()=>{
         let sum : number = mathOpreations();
 
         expect(sum).toEqual(5);
-    });
-
-    test("should validateOperends take input value 5 as string  and check input is number and return 5 as number",()=>{
-        let input : string = "5";
-
-        let result : number = validateOprends(input,"no operations will be performed",1,0) as number;
-
-        expect(result).not.toEqual("5");
-        expect(result).toEqual(5);
-    });
-
-    test("should not accept non integer like 'hi', 'hello', contiuously prompt user to give valid input '5' and return 5 as number.",()=>{
-        let initialInput : string = 'hi';
-        mockedPrompt    
-            .mockReturnValueOnce("hello")
-            .mockReturnValueOnce("5");
-
-        let result : number = validateOprends(initialInput,"no operations will be performed",1,0) as number;
-
-        expect(result).toEqual(5);
-            
-    });
-
-    test("should should take input value as 'exit' and return 'exit'",()=>{
-        let input : string = "exit";
-
-        let result : string = validateOprends(input,"no operations will be performed",1,0) as string;
-
-        expect(result).toEqual("exit");
-    });
-
-    test("should take multiple invalid inputs 'Anas' and 'Mohammed' and take valid input 'exit' and return 'exit'",()=>{
-        let initialInput : string = 'Anas';
-        mockedPrompt
-            .mockReturnValueOnce("Mohammed")
-            .mockReturnValueOnce("exit");
-
-        let result : string = validateOprends(initialInput,"no operations will be performed",1,0) as string;
-
-        expect(result).toEqual("exit");
     });
 
 })
