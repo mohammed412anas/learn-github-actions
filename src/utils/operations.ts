b58f33b738b420aded8a8ce2d40cea750cb65a17
mohammed412anas <mohammed.anas@everest.engineering>
2025-07-29 15:28:49 +0530

    Update Operations class with multiplication method to myltiply given input values

--- src/utils/operations.ts
+++ src/utils/operations.ts
@@ -5,9 +5,13 @@ export class Operations {
     };
 
     substraction(a: number, b: number): number {
-        if(a===0){
+        if(a === 0){
             return b-a;
-        }
+        };
         return a - b;
     };
-}
+
+    multiplication(a: number, b: number): number {
+        return a * b;
+    };
+};
