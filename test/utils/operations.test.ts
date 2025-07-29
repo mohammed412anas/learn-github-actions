cfe50e145223bd646899742af6cb50581339652c
mohammed412anas <mohammed.anas@everest.engineering>
2025-07-29 14:17:35 +0530

    Add unit test for substraction method of class Operations to verify it returns different between two numbers

--- test/utils/operations.test.ts
+++ test/utils/operations.test.ts
@@ -1,11 +1,19 @@
 import { Operations } from "../../src/utils/operations";
 
 describe("test suit of operation.ts module",()=>{
+    let calculation : Operations;
+    beforeEach(()=>{
+        calculation = new Operations;
+    })
     test("should addition method of class Operations add '2' and '3' and return '5'",()=>{
-        let calculation = new Operations()
-
         let add : number = calculation.addition(2,3);
 
         expect(add).toEqual(5)
-    })
-})
+    });
+
+    test("should substraction method of class Operations substract '3' from '2' and return '1'",()=>{
+        let add : number = calculation.substraction(2,3);
+
+        expect(add).toEqual(-1);
+    });
+});
